bf8c3b340ab76f0f449854ec62e6cf073babdb2d
Antoni Castejón <antonicg@gmail.com>
2018-03-01 20:57:12 +0100

    Extending CryptoListFragment from ViewLifecycleFragment and using his lifecycleOwner to subscribe to state liveData

--- app/src/main/java/com/antonicastejon/cryptodata/presentation/main/crypto_list/CryptoListFragment.kt
+++ app/src/main/java/com/antonicastejon/cryptodata/presentation/main/crypto_list/CryptoListFragment.kt
@@ -5,7 +5,6 @@ import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +12,7 @@ import android.view.ViewGroup
 import com.antonicastejon.cryptodata.R
 import com.antonicastejon.cryptodata.domain.LIMIT_CRYPTO_LIST
 import com.antonicastejon.cryptodata.presentation.common.CryptoListRecyclerAdapter
+import com.antonicastejon.cryptodata.presentation.common.ViewLifecycleFragment
 import com.antonicastejon.cryptodata.presentation.widgets.paginatedRecyclerView.PaginationScrollListener
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.crypto_list_fragment.*
@@ -25,7 +25,7 @@ private val TAG = CryptoListFragment::class.java.name
 
 fun newCryptoListFragment() = CryptoListFragment()
 
-class CryptoListFragment : Fragment() {
+class CryptoListFragment : ViewLifecycleFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -66,24 +66,6 @@ class CryptoListFragment : Fragment() {
         super.onAttach(context)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel = ViewModelProviders.of(this, viewModelFactory).get(CryptoListViewModel::class.java)
-        observeViewModel()
-        savedInstanceState?.let {
-            viewModel.restoreCryptoList()
-        } ?: viewModel.updateCryptoList()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        viewModel.stateLiveData.removeObserver(stateObserver)
-    }
-
-    private fun observeViewModel() {
-        viewModel.stateLiveData.observe(this, stateObserver)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.crypto_list_fragment, container, false)
         initializeToolbar(view)
@@ -107,6 +89,25 @@ class CryptoListFragment : Fragment() {
 
     private fun initializeSwipeToRefreshView(view:View) {
         view.swipeRefreshLayout.setOnRefreshListener { viewModel.resetCryptoList() }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(CryptoListViewModel::class.java)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        observeViewModel()
+        savedInstanceState?.let {
+            viewModel.restoreCryptoList()
+        } ?: viewModel.updateCryptoList()
+    }
+
+    private fun observeViewModel() {
+        getFragmentLifecycleOwner()?.let {
+            viewModel.stateLiveData.observe(it, stateObserver)
+        }
     }
 
     private fun loadNextPage() {
